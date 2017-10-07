c98fa6a8e452c66571e41572a5f6f0fd6e30702d
a01204421 <a01204421@itesm.mx>
2017-10-07 03:00:27 -0500

    Add custom messages when raise errors

---
+++ app/lib/message.rb
@@ -0,0 +1,40 @@
+# This class returns a message to authentication request
+class Message
+
+  def self.not_found(record = 'record')
+    "Sorry, #{record} not found."
+  end
+
+  def self.invalid_credentials
+    'Invalid credentials'
+  end
+
+  def self.invalid_token
+    'Invalid token'
+  end
+
+  def self.missing_token
+    'Missing token'
+  end
+
+  def self.unauthorized
+    'Unauthorized request'
+  end
+
+  def self.account_created
+    'Account created successfully'
+  end
+
+  def self.account_not_created
+    'Account could not be created'
+  end
+
+  def self.expired_token
+    'Sorry, your token has expired. Please login to continue.'
+  end
+
+  def self.signature_has_expired
+    'Signature has expired'
+  end
+
+end
