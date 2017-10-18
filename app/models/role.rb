8857cbb1b0122cd2c36234ac1b19311a1fbb67ca
a01204421 <a01204421@itesm.mx>
2017-10-17 22:51:33 -0500

    Add unit test for Role attributes

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,14 @@
 class Role < ApplicationRecord
 
+  validates :name,
+    presence: true,
+    length:   { maximum: 32 }
+
+  validates :description,
+    presence: true,
+    length:   { maximum: 64 }
+
+  validates :status,
+    inclusion: { in: [true, false] }
 
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject(:role) do
+    # Returns a User instance that's not saved
+    FactoryGirl.build(:role)
+  end
+
+  # Test for all valid attributes are given
+  it 'is valid if the name, description and status are given' do
+    expect(role).to be_valid
+  end
+
+  # Set of tests for each attribute that is required
+  it 'is not valid without a name' do
+    role.name = nil
+
+    expect(role).not_to be_valid
+  end
+
+  it 'is not valid without a description' do
+    role.description = nil
+
+    expect(role).not_to be_valid
+  end
+
+  it 'is not valid without a status' do
+    role.status = nil
+
+    expect(role).not_to be_valid
+  end
+
+  # Set of tests to validate the length for each attribute
+  it 'is not valid if the name given contains more than 32 characters' do
+    role.name = 'a' * 33
+
+    expect(role).not_to be_valid
+  end
+
+  it 'is not valid if the description given contains more than 64 characters' do
+    role.description = 'a' * 65
+
+    expect(role).not_to be_valid
+  end
+
 end
