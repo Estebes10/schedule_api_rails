3ec1ecc3b4d5a27e2ab9c5267f4056ff07948eb4
a01204421 <a01204421@itesm.mx>
2017-10-18 23:45:42 -0500

    Refactor in method to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,20 @@ class UsersController < ApplicationController
   # skip authorization request to users who are trying to create their account
   skip_before_action :authorize_request, only: :create
 
+  before_action :get_student_role,
+    only: :create
+
+  after_action :create_assigment,
+    only: :create
+
   # POST /signup
   # return authenticated token upon signup
   def create
     # Create the user if valid attributes are given
-    if user = User.create!(user_params)
-      auth_token = AuthenticateUser.new(user.email, user.password).call
+    if @user = User.create!(user_params)
+      auth_token = AuthenticateUser.new(@user.email, @user.password).call
       response = { message: Message.account_created, auth_token: auth_token }
       json_response(response, :created)
-      role = Role.find_by(code: 'Student')
-      Assignment.create(role: role, user: user)
     else
       json_response(response, :unprocessable_entity)
     end
@@ -35,6 +39,16 @@ class UsersController < ApplicationController
       :birthday,
       :status,
     )
+  end
+
+  # Get the student role before create Assignment
+  def get_student_role
+    @role = Role.find_by(code: 'Student')
+  end
+
+  # Create Assignment after save the new user
+  def create_assigment
+    Assignment.create(role: @role, user: @user)
   end
 
 end
