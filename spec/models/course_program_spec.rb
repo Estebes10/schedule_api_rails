acf7fdc4b61c760cbb2e0d94d7518b6cee872515
a01204421 <a01204421@itesm.mx>
2017-10-20 19:27:21 -0500

    Create test for CourseProgram association

---
+++ spec/models/course_program_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe CourseProgram, type: :model do
+
+  it "belong to courses" do
+    assc = described_class.reflect_on_association(:course)
+    expect(assc.macro).to eq(:belongs_to)
+  end
+
+  it "belong to study programs" do
+    assc = described_class.reflect_on_association(:study_program)
+    expect(assc.macro).to eq(:belongs_to)
+  end
+
+end
