61ba338ebb51bc0117a6ab3f21f164cf1630c044
a01204421 <a01204421@itesm.mx>
2017-10-24 18:23:09 -0500

    Create endpoint for task to create a new course

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,18 @@ Rails.application.routes.draw do
   # Endpoint to create new student user
   post 'signup', to: 'users#create'
 
+  # enpoints following /api/
   namespace :api do
 
+    # enpoints following /api/v1/
     namespace :v1 do
 
       resources :study_programs,
         only:  [:create]
+
+      # enpoints following /api/v1/courses
+      resources :courses,
+        only: [:create]
 
     end
 
