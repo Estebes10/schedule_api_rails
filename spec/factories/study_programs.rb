8fddaf16c20396fc04f17c51ac9a045fbd4ee865
a01204421 <a01204421@itesm.mx>
2017-10-24 15:49:00 -0500

    Change factory to create sequences for list of objects to tests requests

--- spec/factories/study_programs.rb
+++ spec/factories/study_programs.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   # Generate a fake record for StudyProgram model
   factory :study_program do
-    name          Faker::App.name
+    sequence(:name) { |n| "#{Faker::App.name}#{n}" }
     description   Faker::Lorem.characters(63)
     status        true
   end
