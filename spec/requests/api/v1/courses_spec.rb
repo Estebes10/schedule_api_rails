2431bac9a673ec4d3ba539808daa13e8ae746763
a01204421 <a01204421@itesm.mx>
2017-10-24 18:22:42 -0500

    Add request tests to create a new course

---
+++ spec/requests/api/v1/courses_spec.rb
@@ -0,0 +1,64 @@
+# spec/requests/todos_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Courses API', type: :request do
+
+  # create a user to test
+  let(:user) { create(:user) }
+
+  # authorize request
+  let(:headers) { valid_headers }
+
+  # Test suite for POST /study_programs
+  describe 'POST /api/v1/courses' do
+
+    # valid payload
+    let(:valid_attributes) do
+      {
+        course: {
+          name:        'Programación avanzada',
+          code:        'TC1010F',
+          description: 'Materia para programar de Pedro XD',
+          units:       8,
+          class_hours: 3,
+          lab_hours:   0,
+          status:      true,
+        }
+      }.to_json
+    end
+
+    context 'when the request is valid' do
+
+      before { post '/api/v1/courses', params: valid_attributes, headers: headers }
+
+      it "returns the course created" do
+        course = Course.last
+
+        expect(course.name).to eq('Programación avanzada')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+
+    end
+
+    context 'when the request is invalid' do
+
+      # Only one required attribute is given
+      before { post '/api/v1/courses', params: { name: 'ISC18' }.to_json, headers: headers }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: Code can't be blank, Units can't be blank, Class hours can't be blank, Lab hours can't be blank/)
+      end
+
+    end
+
+  end
+
+end
