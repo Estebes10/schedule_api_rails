39904c5ef92d5db4ebc9e29e30a3c7338a748955
a01204421 <a01204421@itesm.mx>
2017-10-24 15:44:27 -0500

    Create requests to get study programs

--- spec/requests/api/v1/study_programs_spec.rb
+++ spec/requests/api/v1/study_programs_spec.rb
@@ -6,8 +6,59 @@ RSpec.describe 'Study Programs API', type: :request do
   # create a user to test
   let(:user) { create(:user) }
 
+  # Initialize 10 records of courses
+  before(:each) do
+    @total = 10
+  end
+
+  let!(:study_programs) { create_list(:study_program, @total) }
+
   # authorize request
   let(:headers) { valid_headers }
+
+  # Test suite for GET /api/v1/study_programs
+  describe 'GET /api/v1/study_programs' do
+
+    context 'when study programs exist' do
+
+      # make HTTP get request before each example
+      before { get '/api/v1/study_programs', params: {}, headers: headers}
+
+      it 'returns study programs' do
+        # Note `json` is a custom helper to parse JSON responses
+        expect(json).not_to be_empty
+      end
+
+      it 'return 10 records' do
+        expect(json.size).to eq(@total)
+      end
+
+      it 'returns ok status' do
+        expect(response).to have_http_status(200)
+      end
+
+    end
+
+    context 'when study programs does not exists' do
+
+      before(:each) do
+        StudyProgram.destroy_all
+      end
+
+      # make HTTP get request before each example
+      before { get '/api/v1/study_programs', params: {}, headers: headers}
+
+      it 'not returns study programs' do
+        expect(json).to be_empty
+      end
+
+      it 'returns an unprocessable entity status' do
+        expect(response).to have_http_status(200)
+      end
+
+    end
+
+  end
 
   # Test suite for POST /study_programs
   describe 'POST /api/v1/study_programs' do
