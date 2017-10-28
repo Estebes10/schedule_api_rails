ff026be95607d6f90536a475c74225f7c2c6dc77
a01204421 <a01204421@itesm.mx>
2017-10-27 23:49:01 -0500

    Create endpoint to remove a study program

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       resources :study_programs,
-        only:  [:create, :index, :update]
+        only:  [:create, :index, :update, :destroy]
 
       # endpoints following /api/v1/courses
       resources :courses,
