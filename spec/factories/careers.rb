bde872c24986660d259bdc2d620671a0db38b349
a01204421 <a01204421@itesm.mx>
2017-10-28 01:09:45 -0500

    Create generator of fake data for Career model to tests

---
+++ spec/factories/careers.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :career do
+    sequence(:name) { |n| "#{Faker::Company.profession}#{n}" }
+    sequence(:code) { |n| "#{Faker::Code.asin}#{n}" }
+    description     Faker::Company.catch_phrase
+    status          Faker::Boolean.boolean
+  end
+
+end
