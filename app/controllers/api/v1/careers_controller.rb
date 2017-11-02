9300769bdf1cd16a70766ac301088bf8a5649f5e
a01204421 <a01204421@itesm.mx>
2017-11-02 16:42:33 -0600

    Implement the method to update a career record

--- app/controllers/api/v1/careers_controller.rb
+++ app/controllers/api/v1/careers_controller.rb
@@ -1,7 +1,15 @@
 module Api::V1
 
+  # This class has the methods necessaries for endpoints requests required for
+  # Career model
+
   class CareersController < ApplicationController
 
+    # Get the career before update
+    before_action :get_career,
+      only: :update
+
+    # GET /api/v1/careers/
     def index
       # Get all careers
       @careers = Career.all
@@ -15,6 +23,7 @@ module Api::V1
       end
     end
 
+    # POST /api/v1/careers/
     def create
       if @career = Career.create!(creation_attributes)
         response = {
@@ -27,8 +36,29 @@ module Api::V1
       end
     end
 
+    # PUT /api/v1/careers/:id
+    def update
+      if @career
+        # try to update the record with the attributes permited
+        if @career.update(creation_attributes)
+          response = {
+            message: Message.record_updated(@career.class.name),
+            career: @career,
+          }
+          json_response(response)
+        # response with 422 if is imposible to update the career
+        else
+          json_response(@career, :unprocessable_entity)
+        end
+      # return 404 when the id of the career is not valid
+      else
+        json_response(@career, :not_found)
+      end
+    end
+
     private
 
+    # attributes permited to create and update one career
     def creation_attributes
       params.permit(
         :name,
@@ -37,6 +67,11 @@ module Api::V1
         :status,
         :department_id,
       )
+    end
+
+    # Get the career with the ID sent in the request
+    def get_career
+      @career = Career.find(params[:id])
     end
 
   end
