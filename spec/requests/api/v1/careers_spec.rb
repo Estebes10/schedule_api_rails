3d1b5f368b08ba532ccc37da16bffc233836c266
a01204421 <a01204421@itesm.mx>
2017-11-01 19:17:16 -0600

    Add request tests to add a new career

--- spec/requests/api/v1/careers_spec.rb
+++ spec/requests/api/v1/careers_spec.rb
@@ -59,4 +59,68 @@ RSpec.describe 'careers API', type: :request do
 
   end
 
+  # Test suite for POST /careers
+  describe 'POST /api/v1/careers' do
+
+    # valid payload
+    let(:valid_attributes) do
+      {
+        name:        'Ingeniería en Sistemas Computacionales',
+        code:        'ISC',
+        description: 'Esta es una carrera impartida en el campus querétaro',
+        status:      true,
+      }.to_json
+    end
+
+    context 'when the request is valid' do
+
+      before { post '/api/v1/careers', params: valid_attributes, headers: headers }
+
+      it "adds a new record" do
+        expect(Career.count).to eq(@total + 1)
+      end
+
+      it 'returns the career created' do
+        career = Career.last
+
+        expect(career.name).to eq('Ingeniería en Sistemas Computacionales')
+      end
+
+      it 'contains the code' do
+        career = Career.last
+
+        expect(career.code).to eq('ISC')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+
+    end
+
+    context 'when the request is invalid' do
+
+      # Not all required attributes are given
+      let(:invalid_attributes) do
+        {
+          name:   Faker::Educator.course,
+          status: true,
+        }.to_json
+      end
+
+      before { post '/api/v1/careers', params: invalid_attributes, headers: headers }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: Code can't be blank/)
+      end
+
+    end
+
+  end
+
 end
