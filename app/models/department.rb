d5e13e8905371e4897b04daf79366ba31324e5d7
a01204421 <a01204421@itesm.mx>
2017-11-01 22:34:04 -0600

    Add validation to attributes and associations in Department model

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,25 @@
 class Department < ApplicationRecord
+
+  # Associations
   belongs_to :campu
+
+  has_many :careers
+
+  # validate attributes
+  validates :name,
+    presence:   true,
+    length:     { maximum: 64 },
+    uniqueness: true
+
+  validates :code,
+    presence:   true,
+    length:     { maximum: 16 },
+    uniqueness: true
+
+  validates :description,
+    length: { maximum: 512 }
+
+  validates :status,
+    inclusion: { in: [true, false] }
+
 end
