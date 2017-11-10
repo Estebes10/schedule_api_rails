5d29b5113bdbae91d5ddfb944cef713da5912057
a01204421 <a01204421@itesm.mx>
2017-11-09 23:30:38 -0600

    Add tests to validate associations between Semester and Courses

---
+++ spec/models/semester_course_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe SemesterCourse, type: :model do
+
+  # Test associations
+  it "belong to courses" do
+    assc = described_class.reflect_on_association(:course)
+    expect(assc.macro).to eq(:belongs_to)
+  end
+
+  it "belong to semesters" do
+    assc = described_class.reflect_on_association(:semester)
+    expect(assc.macro).to eq(:belongs_to)
+  end
+
+end
